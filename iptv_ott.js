2ed96345c218e108604b4ccd9a121ea40dce6396
Artrax90 <67760877+Artrax90@users.noreply.github.com>
2026-02-07 20:11:36 +0300

    Update iptv_ott.js

--- iptv_ott.js
+++ iptv_ott.js
@@ -1,7 +1,7 @@
 // ==Lampa==
 // name: IPTV Lite
-// version: 1.0.4
-// description: IPTV плеер по стандартам Lampa Store
+// version: 1.0.6
+// description: IPTV плеер (исправлена ошибка Settings.add)
 // author: Gemini
 // ==/Lampa==
 
@@ -16,10 +16,11 @@
 
         this.create = function () {
             var _this = this;
+            // Используем Lampa.Storage.get напрямую
             var url = Lampa.Storage.get('iptv_m3u_link', '');
 
             if (!url) {
-                items.append('<div class="empty" style="text-align:center;padding:40px">Укажите ссылку на M3U в настройках Lampa (Раздел IPTV Lite)</div>');
+                items.append('<div class="empty" style="text-align:center;padding:40px">Укажите ссылку на M3U в настройках (Раздел IPTV Lite)</div>');
             } else {
                 Lampa.Loading.show();
                 network.silent(url, function (str) {
@@ -62,12 +63,9 @@
         this.renderGroups = function () {
             var _this = this;
             items.empty();
-            
             Object.keys(groups).forEach(function (gName) {
                 var card = Lampa.Template.get('button_category', {title: gName + ' (' + groups[gName].length + ')'});
-                card.on('hover:enter', function () {
-                    _this.renderChannels(gName);
-                });
+                card.on('hover:enter', function () { _this.renderChannels(gName); });
                 items.append(card);
             });
             Lampa.Controller.enable('content');
@@ -76,7 +74,6 @@
         this.renderChannels = function (gName) {
             var _this = this;
             items.empty();
-            
             var back = Lampa.Template.get('button_category', {title: '[ Назад ]'});
             back.on('hover:enter', function () { _this.renderGroups(); });
             items.append(back);
@@ -101,50 +98,52 @@
         };
     }
 
-    function init() {
-        // Регистрация экрана
-        window.Lampa.Component.add('iptv_lite', IPTVPlugin);
+    function startPlugin() {
+        // Регистрация компонента
+        Lampa.Component.add('iptv_lite', IPTVPlugin);
 
-        // Добавление настроек
-        Lampa.Settings.main && Lampa.Settings.main({
-            id: 'iptv_lite_settings',
+        // Добавляем пункт в меню
+        Lampa.Menu.add({
+            id: 'iptv_lite',
             title: 'IPTV Lite',
-            icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M21 7V5a2 2 0 0 0-2-2H5a2 2 0 0 0-2-2v2a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2z" fill="white"/></svg>'
+            icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2z" fill="white"/></svg>',
+            section: 'main',
+            onSelect: function() {
+                Lampa.Activity.push({ title: 'IPTV Lite', component: 'iptv_lite', page: 1 });
+            }
         });
 
-        Lampa.Settings.add({
-            name: 'iptv_m3u_link',
-            section: 'iptv_lite_settings',
-            title: 'M3U Плейлист URL',
-            type: 'input',
-            placeholder: 'http://...',
-            default: ''
+        // ПРАВИЛЬНАЯ регистрация настроек через Params
+        Lampa.Params.select('iptv_m3u_link', '', 'M3U Link'); // Резервируем имя переменной
+        
+        // Создаем секцию в настройках
+        Lampa.Template.add('settings_iptv_lite', '<div><div class="settings-param selector" data-name="iptv_m3u_link" data-type="input"><div class="settings-param__name">M3U Плейлист URL</div><div class="settings-param__value"></div><div class="settings-param__descr">Введите ссылку на .m3u файл</div></div></div>');
+
+        Lampa.Settings.listener.follow('open', function (e) {
+            if (e.name === 'main') {
+                var item = $('<div class="settings-folder selector" data-component="iptv_lite_settings"><div class="settings-folder__icon"><svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M21 7V5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v2a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2z" fill="white"/></svg></div><div class="settings-folder__name">IPTV Lite</div></div>');
+                item.on('hover:enter', function () {
+                    Lampa.Settings.create('iptv_lite_settings', 'IPTV Lite');
+                    Lampa.Settings.add(Lampa.Template.get('settings_iptv_lite', {}));
+                    Lampa.Controller.enable('settings_iptv_lite');
+                });
+                e.body.find('.settings-main').append(item);
+            }
         });
-
-        // Добавление кнопки в меню
-        var menu_item = $('<li class="menu__item selector" data-action="iptv_lite">' +
-            '<div class="menu__ico">' +
-            '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2z" fill="white"/></svg>' +
-            '</div>' +
-            '<div class="menu__text">IPTV Lite</div>' +
-            '</li>');
-
-        menu_item.on('hover:enter', function () {
-            Lampa.Activity.push({
-                title: 'IPTV Lite',
-                component: 'iptv_lite',
-                page: 1
-            });
-        });
-
-        $('.menu .menu__list').append(menu_item);
     }
 
-    // Ожидание готовности по стандарту Store
-    if (window.app_ready) init();
-    else {
-        Lampa.Listener.follow('app', function (e) {
-            if (e.type == 'ready') init();
+    if (window.Lampa) {
+        Lampa.Extensions.add({
+            id: 'iptv_lite',
+            type: 'plugin',
+            onInit: function () {
+                if (window.app_ready) startPlugin();
+                else {
+                    Lampa.Listener.follow('app', function (e) {
+                        if (e.type == 'ready') startPlugin();
+                    });
+                }
+            }
         });
     }
 })();
